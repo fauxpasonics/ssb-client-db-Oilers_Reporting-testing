a46dfa358a5753a864d0126fc5be0e3ca468483b
Luke Miles <lmiles@ssbinfo.com>
2018-08-03 20:40:25 -0400

    Initial Commit

---
+++ Views/prodcopy.vw_Account.sql
@@ -0,0 +1,8 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+ CREATE VIEW [prodcopy].[vw_Account] AS 
+						---- CREATED BY PROCESS ON May  5 2017 10:19PM
+						Select * from ProdCopy.Account Where 1=1 
+GO
---
+++ Views/prodcopy.vw_Contact.sql
@@ -0,0 +1,12 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+CREATE VIEW [prodcopy].[vw_Contact] AS
+ 
+SELECT *
+from ProdCopy.Contact Where 1=1 
+
+
+GO
